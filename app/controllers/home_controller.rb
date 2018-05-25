9f73565ba70c7c296be4ffd1b97003b04aec727e
sbaixas <sbaixas@gmail.com>
2018-05-25 19:47:25 -0400

    uresolved conflicts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+  	if user_signed_in?
+  		if current_user.user_type == "generic"
+  			@created_projects = current_user.created
+  		end
+  	end
   end
 end
