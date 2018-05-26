6c3577867eef60a8db531170e0b31d79cfda3088
monty <iacarril@miuandes.cl>
2018-05-25 23:49:54 -0400

    admin category management

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :categories
 	resources :projects do
 		resources :promises
-	end
+  end
 	get 'project/show_project'
-
+	get 'admin_categories' => 'admin#admin_categories'
 	devise_for :users
 	get 'user/show_owned'
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
