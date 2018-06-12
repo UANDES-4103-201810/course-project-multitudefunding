7def8c27696609ddd04651f551003a233e4e2e66
sbaixas <sbaixas@gmail.com>
2018-06-12 14:33:14 -0400

    showing money

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   # has_many :categorizations
   # has_many :projects, through: :categorizations
-  has_many :projects
+  has_many :projects , :through => :categories_projects, source: :project, dependent: :delete_all
   validates :name, presence: true
 end
